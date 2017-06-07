5e73563d1a4e2a6fe355a2d20b83a4f6b461541e
Nealon Young <hi@nealyoung.me>
2017-06-07 14:12:55 -0700

    Create new Xcode project

---
+++ HackerFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerFeed
+//
+//  Created by Nealon Young on 6/7/17.
+//  Copyright © 2017 Nealon Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
